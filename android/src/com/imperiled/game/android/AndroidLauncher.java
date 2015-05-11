15a49a40ad0089e60f90b3fcca7b445ba3a450c8
Jonas Wedin <jonas@lechuck.se>
2015-05-11 15:09:33 +0200

    initial commit from generated source

---
+++ android/src/com/imperiled/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.imperiled.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.imperiled.game.Imperiled;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Imperiled(), config);
+	}
+}
