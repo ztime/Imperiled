cda9e3ab54251175027bbd916e90459486722ea5
John Wikman <john.wikman@hotmail.com>
2015-05-11 22:49:28 +0200

    Ye

--- core/src/com/imperiled/game/PropertyHandler.java
+++ core/src/com/imperiled/game/PropertyHandler.java
@@ -1,6 +1,17 @@
 package com.imperiled.game;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PropertyHandler {
 	public static final String[] eventReqs = {"name", "type", "action"};
 	public static final String[] musicReqs = {"name", "filename"};
+	public static HashMap<String, Actor> currentActors;
+	
+	public static void newActors(ArrayList<Actor> actors) {
+		currentActors = new HashMap<String, Actor>();
+		for(Actor actor : actors) {
+			currentActors.put("hghghj", actor);
+		}
+	}
 }
