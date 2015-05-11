909deaae731c761e5b6e568a4bed8bd3325927c8
Jonas Wedin <jonas@lechuck.se>
2015-05-11 15:47:47 +0200

    added logo

--- core/src/com/imperiled/game/Imperiled.java
+++ core/src/com/imperiled/game/Imperiled.java
@@ -1,27 +1,29 @@
 package com.imperiled.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class Imperiled extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Imperiled extends Game {
+	
+	//Here we can add variables available between game screens
+	//and game states. Should not be used unless we must.
+	
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// we simply switch to main manu screen
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+	}
+	
+	/**
+	 * This should clear any "global" game 
+	 * variables if it needs to 
+	 */
+	public void dispose(){
+		
 	}
 }
