11808c770adcfe2f2e238c986a08464405c8ba9e
John Wikman <john.wikman@hotmail.com>
2015-05-12 16:31:44 +0200

    bee will turn now

--- core/src/com/imperiled/game/Direction.java
+++ core/src/com/imperiled/game/Direction.java
@@ -2,4 +2,17 @@ package com.imperiled.game;
 
 public enum Direction {
 	UP,DOWN,RIGHT,LEFT;
+	
+	private Direction opposite;
+	
+	static {
+		UP.opposite = DOWN;
+		DOWN.opposite = UP;
+		LEFT.opposite = RIGHT;
+		RIGHT.opposite = LEFT;
+	}
+	
+	public Direction getOpposite() {
+		return opposite;
+	}
 }
