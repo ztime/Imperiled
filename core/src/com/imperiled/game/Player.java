f0c82252745406cc6fb654f784bddbe78237e01d
John Wikman <jwikman@kth.se>
2015-05-17 21:46:58 +0200

    map tweaks

--- core/src/com/imperiled/game/Player.java
+++ core/src/com/imperiled/game/Player.java
@@ -63,7 +63,7 @@ public class Player extends Actor {
 		Rectangle newRect = new Rectangle();
 		newRect.x = this.x + this.getWidth() / 4;
 		newRect.y = this.y;
-		newRect.height = (3 * this.getHeight()) / 7;
+		newRect.height = (3 * this.getHeight()) / 9;
 		newRect.width = this.getWidth() / 2;
 		return newRect;
 	}
