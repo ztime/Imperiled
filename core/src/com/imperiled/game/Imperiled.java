30fef3c178b0fe310dc625905ce37f482ccba673
rullstolsroger <rullstolsroger@users.noreply.github.com>
2016-04-16 00:58:03 +0200

    License
    Added license!

--- core/src/com/imperiled/game/Imperiled.java
+++ core/src/com/imperiled/game/Imperiled.java
@@ -1,3 +1,10 @@
+/*
+	Copyright © 2015 Jonas Wedin jonwed@kth.se John Wikman jwikman@kth.se
+	This work is free. You can redistribute it and/or modify it under the
+	terms of the Do What The Fuck You Want To Public License, Version 2,
+	as published by Sam Hocevar. See the COPYING file for more details.
+*/
+
 package com.imperiled.game;
 
 import com.badlogic.gdx.Game;
